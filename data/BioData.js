7317c7db00791b14e54d26ce7797e48b4db38f2d
h4MeMengoding <ilhamshofaa@gmail.com>
2023-12-14 20:34:17 +0700

    deploy

---
+++ data/BioData.js
@@ -0,0 +1,27 @@
+// date: 1 Aug, 2022
+const bioData = [
+    {
+        name: 'meriros vo',
+        // username: 'nxtlnk.vercel.app',
+        url: 'https://nxtlnk.vercel.app',
+        titleImg: true,
+        avatar: '/meriros.png',
+        nftAvatar: true,
+        description: 'Looking your photo? ',
+        descShow: true,
+        subdesc: 'Search by class and sort by date!',
+        subdescShow: true,
+        newProductUrl: 'https://3dicons.co',
+        newProduct: true,
+        author: 'Ilham Shofa',
+        endAuthor: 'Meriros Yerbook 24',
+        // authorURL: 'https://twitter.com/realvjy',
+        footerText: 'made by',
+        endText: 'for'
+    },
+];
+
+export default bioData;
+
+
+
---
+++ data/LinksData.js
@@ -0,0 +1,115 @@
+// date 29-Jul, 2022
+const webLinks = [
+    // All social profile
+    {
+        title: 'Instagram',
+        url: 'https://instagram.com/',
+        type: 'social',
+        icon: '/insta.svg',
+        on: true
+    },
+    {
+        title: 'Youtube',
+        url: 'https://youtube.com/realvjy',
+        type: 'social',
+        icon: '/youtube.svg',
+        on: false
+    },
+    // Photos section
+    {
+        title: 'XII MIPA 1',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII MIPA 2',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII MIPA 3',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII MIPA 4',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII MIPA 5',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII MIPA 6',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII MIPA 7',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII IPS 1',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII IPS 2',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII IPS 3',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII IPS 4',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+    {
+        title: 'XII IPS 5',
+        url: '',
+        type: 'photos',
+        icon: '/photos.svg',
+        on: true
+    },
+
+    {
+        title: 'CAS (Soon)',
+        url: 'https://www.youtube.com/',
+        type: 'other',
+        icon: '/yt-white.png',
+        on: true
+    },
+];
+
+export default webLinks;
+
+
