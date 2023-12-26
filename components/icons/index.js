754324083f8b5b9a319a8a45b181ef754be56f95
h4MeMengoding <ilhamshofaa@gmail.com>
2023-12-26 08:38:12 +0700

    fix bug icon

--- components/icons/index.js
+++ components/icons/index.js
@@ -132,13 +132,13 @@ export const NewUp = (props) => (
 xmlns="http://www.w3.org/2000/svg"
 viewBox="0 0 24 24"
 fill="currentColor"
-class="w-6 h-6"
+classNames="w-6 h-6"
 width={props.width || 18}
 height={props.height || 18}
 className="new-up"
 >
 
-  <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+  <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
 </svg>
 );
 
